e60ce052db41c8cc7169762c33a7b6995c31104c
JavierS084 <javier.sanchezb084@gmail.com>
2021-11-23 15:56:02 -0300

    desustructuracion

---
+++ 02/intro-javascript/src/bases/05-desustructuracion.js
@@ -0,0 +1,50 @@
+//desustructuracion
+
+const persons = {
+
+
+    nombre: 'tony',
+    edad: 45,
+    clave: 'iroman'
+ 
+}
+
+
+/**
+ 
+ console.log(nombre);
+ console.log(edad);
+ console.log(clave);
+ * 
+ */
+
+//const { nombre, edad, clave } = usuario;
+
+
+//desustructuracion
+
+const useContext = ({nombre, edad, clave, rango = 'capitan '}) => {
+     
+
+   // console.log(edad, clave, nombre, rango);
+
+    return{
+        nombreClave: clave,
+        anios: edad,
+        latlng: {
+            lat: 15.222,
+            lng: -5454 
+        }
+    }
+ 
+
+}
+//asignacion desustructurante
+
+
+const {nombreClave, anios, latlng:{ lat, lng} } = useContext( persons );
+
+
+console.log(avenger); 
+console.log(lat, lng);
+//retornoPersona(persons);
